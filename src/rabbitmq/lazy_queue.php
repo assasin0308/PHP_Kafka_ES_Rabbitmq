c9c2a155c2e60a6af2f9d98c7da689ee6b5c53f8
assasin0308 <assasin0308@sina.com>
2024-10-15 15:49:58 +0800

    惰性队列解决mq消息堆积问题

--- src/rabbitmq/lazy_queue.php
+++ src/rabbitmq/lazy_queue.php
@@ -21,14 +21,18 @@ $channel->queue_bind($queue, $exchange,$route_key);
 
 
 
-// 发布消息
-$message = new AMQPMessage( date('Y-m-d H:i:s').' 生产者: Hello, Lazy Queue!');
-$channel->basic_publish($message, $exchange, $route_key);
+// 发布消息 模拟10000条消息堆积
+for($i=1;$i <= 10000; $i++){
+    $message = new AMQPMessage( date('Y-m-d H:i:s')." | 这是第 {$i} 条惰性队列消息! \n");
+    $channel->basic_publish($message, $exchange, $route_key);
+    echo date('Y-m-d H:i:s')." 这是第 {$i} 条惰性队列消息! \n ";
+}
 
-/********************************************************************/
+
+/**********************************Consumer消费消息**********************************/
 
 $callback = function ($msg) {
-    echo  date('Y-m-d H:i:s'). ' 消费者:  Received: ', $msg->body, "\n";
+    echo  date('Y-m-d H:i:s'). ' 消费者收到: ', $msg->body, "\n";
 };
 
 $channel->basic_consume($queue, '', false, true, false, false, $callback);

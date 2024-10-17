6d23840553a6269453c5bec29dc26b4850c2fa03
assasin0308 <assasin0308@sina.com>
2024-10-17 21:33:24 +0800

    Redis高级

--- src/redis/predis.php
+++ src/redis/predis.php
@@ -1,1 +1,14 @@
 <?php
+
+require '../vendor/autoload.php';
+
+use Predis\Client;
+
+$redis = new Client();
+$redis->connect('127.0.0.1',6379);
+$redis->select(0);
+
+# 1. String 简单字符串
+$redis->set('name','zhangsan');
+
+
